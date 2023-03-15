a6d8437e6b6ef07faf23cd4933aa779c7d083d5e
Tejas Shimpi <78267259+ShimpiTejas@users.noreply.github.com>
2023-03-15 23:17:44 +0530

    Add files via upload

---
+++ call.js
@@ -0,0 +1,18 @@
+
+
+
+function submitForm()
+{
+    //EgaEfYhkq5ndXg37RwU5YzMSd4oJXVQHswJrHFTzFrgm my wallet hash
+
+
+    var solanaPublicKey = document.getElementById("inputText").value;
+
+    const connection 
+
+
+    const connection = new Connection("https://api.devnet.solana.com");
+
+    const inputText = document.getElementById("inputText").value;
+    alert("You Entered : " + inputText);
+}
